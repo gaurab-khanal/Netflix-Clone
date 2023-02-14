b694eefa2fd2eca69f66b835c34fa8245553d722
Gaurab Khanal <khanalgaurab98@gmail.com>
2023-02-14 21:56:20 +0545

    Project Completed

---
+++ assets/js/script.js
@@ -0,0 +1,70 @@
+// QUESTION 1
+const targetDiv = document.getElementById('ans1');
+const btn = document.getElementById('q1');
+btn.addEventListener('click',()=>{
+	if(targetDiv.style.display!=="none"){
+		targetDiv.style.display = "none";
+	}else{
+		targetDiv.style.display = "block";
+	}
+})
+
+// QUESTION 2
+const qn2 = document.getElementById('ans2');
+const qn2btn = document.getElementById('q2');
+
+qn2btn.addEventListener('click',()=>{
+	if(qn2.style.display!=="none"){
+		qn2.style.display = "none";
+	}else{
+		qn2.style.display = "block";
+	}
+})
+
+// QUESTION 3
+const qn3 = document.getElementById('ans3');
+const qn3btn = document.getElementById('q3');
+
+qn3btn.addEventListener('click',()=>{
+	if(qn3.style.display!=="none"){
+		qn3.style.display = "none";
+	}else{
+		qn3.style.display = "block";
+	}
+})
+
+// QUESTION 4
+
+const qn4 = document.getElementById('ans4');
+const qn4btn = document.getElementById('q4');
+
+qn4btn.addEventListener('click',()=>{
+	if(qn4.style.display!=="none"){
+		qn4.style.display = "none";
+	}else{
+		qn4.style.display = "block";
+	}
+})
+
+// QUETSION 5
+const qn5 = document.getElementById('ans5');
+const qn5btn = document.getElementById('q5');
+
+qn5btn.addEventListener('click',()=>{
+	if(qn5.style.display!=="none"){
+		qn5.style.display = "none";
+	}else{
+		qn5.style.display = "block";
+	}
+})
+// QUETSION 6
+const qn6 = document.getElementById('ans6');
+const qn6btn = document.getElementById('q6');
+
+qn6btn.addEventListener('click',()=>{
+	if(qn6.style.display!=="none"){
+		qn6.style.display = "none";
+	}else{
+		qn6.style.display = "block";
+	}
+})
